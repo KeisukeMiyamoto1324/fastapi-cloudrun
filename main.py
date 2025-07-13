f629fe490cc0b28c52480f3c454ac00d13655a9e
KeisukeMiyamoto1324 <129528528+KeisukeMiyamoto1324@users.noreply.github.com>
2025-07-13 04:49:33 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"data": "Hello"}
+
+@app.get("/echo")
+def echo(text: str = Query(..., description="Text to echo back")):
+    return {"echo": text}
